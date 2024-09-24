e76fb50bee48f7264b68a030b038b728698ac640
Johannes Maier <johannes.maier@mailbox.org>
2024-09-24 22:11:03 +0200

    render moving player as a cirle

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use mlua::prelude::*;
 use raylib::prelude::*;
 
+struct game_state {
+    tick: i16,
+}
+
+struct lua_player {
+    lua_state: Lua,
+    x: i32,
+    y: i32,
+}
+
 fn _main_lua() -> LuaResult<()> {
     let l = Lua::new();
     let mt = l.create_table()?;
@@ -11,12 +21,47 @@ fn _main_lua() -> LuaResult<()> {
     Ok(())
 }
 
+fn draw_line_in_direction(
+    mut d: raylib::drawing::RaylibDrawHandle,
+    x: i32,
+    y: i32,
+    angle: f32,
+    length: f32,
+) {
+    let dx = angle.sin() * length;
+    let dy = angle.cos() * length;
+    d.draw_line(
+        x,
+        y,
+        x + dx.round() as i32,
+        y - dy.round() as i32,
+        Color::RED,
+    );
+}
+
+fn render_player(mut d: raylib::drawing::RaylibDrawHandle, p: &lua_player) {
+    d.draw_circle(p.x, p.y, 25.0, Color::GREENYELLOW);
+}
+
+fn advance_player(p: &mut lua_player) {
+    p.x += 1;
+}
+
 fn main() {
+    let mut player1 = lua_player {
+        lua_state: Lua::new(),
+        x: 30,
+        y: 50,
+    };
+    let mut state = game_state { tick: 0 };
     let (mut rl, thread) = raylib::init().size(400, 400).title("hello world").build();
 
+    rl.set_target_fps(60);
     while !rl.window_should_close() {
+        state.tick += 1;
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::GRAY);
-        d.draw_text("HELLLOOOOOO", 10, 10, 20, Color::BLACK);
+        advance_player(&mut player1);
+        render_player(d, &player1);
     }
 }
