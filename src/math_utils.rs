9c133bde36c85e32ceac5ec73f9d55b5fab1b2c0
Johannes Maier <johannes.maier@mailbox.org>
2024-10-03 21:33:52 +0200

    test normalization of absolute angles

--- src/math_utils.rs
+++ src/math_utils.rs
@@ -1,5 +1,7 @@
-pub const HALF_PI: f32 = crate::PI as f32 / 2.0;
-pub const TWO_PI: f32 = crate::PI as f32 * 2.0;
+use std::f32::consts::PI;
+
+pub const HALF_PI: f32 = PI / 2.0;
+pub const TWO_PI: f32 = PI * 2.0;
 
 pub fn clamp(x: f32, lower: f32, upper: f32) -> f32 {
     f32::min(f32::max(lower, x), upper)
@@ -12,5 +14,46 @@ pub fn normalize_abs_angle(angle: f32) -> f32 {
         normalize_abs_angle(angle + TWO_PI)
     } else {
         angle
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use float_eq::assert_float_eq;
+    use std::f32::consts::PI;
+
+    mod normalize_abs_angle {
+        use super::*;
+
+        #[test]
+        fn angle_greater_than_2pi() {
+            let res = normalize_abs_angle(7.0);
+            assert_float_eq!(res, 0.7168, abs <= 0.0001);
+        }
+
+        #[test]
+        fn angle_greater_than_4pi() {
+            let res = normalize_abs_angle(5.0 * PI);
+            assert_float_eq!(res, PI, abs <= 0.0001);
+        }
+
+        #[test]
+        fn angle_less_than_0() {
+            let res = normalize_abs_angle(-PI);
+            assert_float_eq!(res, PI, abs <= 0.0001);
+        }
+
+        #[test]
+        fn angle_less_than_minus_2pi() {
+            let res = normalize_abs_angle(-5.0 * PI);
+            assert_float_eq!(res, PI, abs <= 0.0001);
+        }
+
+        #[test]
+        fn angle_between_0_and_2pi() {
+            let res = normalize_abs_angle(4.123);
+            assert_float_eq!(res, 4.123, abs <= 0.0001);
+        }
     }
 }
