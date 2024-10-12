0b310a739f0a186d5e1f5c05ca4e9e99d504da9e
Johannes Maier <johannes.maier@mailbox.org>
2024-10-12 11:51:14 +0200

    setup: add stylua

--- players/kai/main.lua
+++ players/kai/main.lua
@@ -3,32 +3,33 @@ local m = {}
 local dir = 1
 
 function m.on_tick(n)
-	if me.head_turn_remaining() == 0 then
-		dir = -dir
-		return { me.turn_head(dir * math.pi) }
-	end
+   if me.head_turn_remaining() == 0 then
+      dir = -dir
+      return { me.turn_head(dir * math.pi) }
+   end
 end
 
 function m.on_round_started(n)
-	print("on round started: " .. n)
+   print("on round started: " .. n)
 end
 
 function m.on_death()
-	print("I'm dead")
+   print("I'm dead")
 end
 
 function m.on_attack_hit(name, x, y)
-	print("Gotcha, " .. name)
+   print("Gotcha, " .. name)
 end
 
 function m.on_enemy_seen(name, x, y)
-	angle = math.atan2(y - me.y(), x - me.x()) + math.pi / 2
-	a = utils.normalize_relative_angle(angle - me.heading())
-	res = { me.turn(a) }
-	if me.turn_remaining() < 0.05 then
-		table.insert(res, me.attack())
-	end
-	return res
+   angle = math.atan2(y - me.y(), x - me.x()) + math.pi / 2
+   a = utils.normalize_relative_angle(angle - me.heading())
+   res = { me.turn(a) }
+   if me.turn_remaining() < 0.05 then
+      print("shooting")
+      table.insert(res, me.attack())
+   end
+   return res
 end
 
 return m
--- players/lloyd/main.lua
+++ players/lloyd/main.lua
@@ -1,15 +1,15 @@
 local m = {}
 
 function m.on_tick(tick)
-	return { me.turn(0.01) }
+   return { me.turn(0.01) }
 end
 
 function m.on_death()
-	print("[lloyd] I'm dead, killed by ?????")
+   print("[lloyd] I'm dead, killed by ?????")
 end
 
 function m.on_hit_by(id)
-	print("HIT OH NO " .. id)
+   print("HIT OH NO " .. id)
 end
 
 return m
