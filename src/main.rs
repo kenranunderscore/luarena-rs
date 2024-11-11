1ff7de2862c16d1f7b467469f24d4a3a2283f1e2
Johannes Maier <johannes.maier@mailbox.org>
2024-11-11 16:53:17 +0100

    poc: embed wasmtime and use a WASM component

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,35 @@
-use std::{
-    path::Path,
-    sync::{
-        atomic::{AtomicBool, Ordering},
-        mpsc::{self, Sender},
-        Arc,
-    },
-    time::Duration,
-};
+use std::path::Path;
+
+use wasmtime::component::bindgen;
 
 mod game;
 mod math_utils;
 mod render;
 mod settings;
 
-use game::*;
-use render::GameRenderer;
-use settings::*;
+bindgen!({
+  inline: r#"
+package foo:stringoperations;
 
-fn run_replay(
-    history_file: &Path,
-    sender: Sender<StepEvents>,
-    delay: &Duration,
-    cancel: &Arc<AtomicBool>,
-) -> Option<()> {
-    let f = std::fs::File::open(history_file).ok()?;
-    let steps: Vec<StepEvents> = ciborium::from_reader(f).ok()?;
-    for step_events in steps {
-        if cancel.load(Ordering::Relaxed) {
-            break;
-        }
-
-        sender
-            .send(step_events)
-            .expect("Failed sending step events");
-        std::thread::sleep(*delay);
-    }
-    Some(())
+interface theint {
+    shmup: func(s: string) -> u32;
 }
 
-fn main() {
-    let (game_writer, game_reader) = mpsc::channel();
-    let cancel = Arc::new(AtomicBool::new(false));
-    let cancel_ref = cancel.clone();
+world theworld {
+    export theint;
+}"#,
+});
 
-    let (mut rl, thread) = raylib::init()
-        .size(WIDTH, HEIGHT)
-        .title("hello world")
-        .msaa_4x()
-        .build();
-
-    let game_thread = std::thread::spawn(move || -> Result<(), GameError> {
-        let mut game = Game::new();
-        game.add_lua_player(Path::new("players/kai"))?;
-        game.add_lua_player(Path::new("players/lloyd"))?;
-
-        let delay = Duration::from_millis(7);
-        // run_replay(Path::new("events"), game_writer, &delay, &cancel_ref).unwrap();
-        run_game(&mut game, &delay, game_writer, &cancel_ref)?;
-        Ok(())
-    });
-
-    let mut renderer = GameRenderer::new(&game_reader);
-    while !rl.window_should_close() && !game_thread.is_finished() {
-        renderer.step(&mut rl, &thread);
-    }
-
-    if game_thread.is_finished() {
-        match game_thread.join().unwrap() {
-            Ok(_) => println!("game finished"),
-            Err(e) => println!("error: {e}"),
-        }
-    } else {
-        cancel.store(true, std::sync::atomic::Ordering::Relaxed);
-        let _ = game_thread.join();
-    }
+fn main() -> wasmtime::Result<()> {
+    let engine = wasmtime::Engine::default();
+    let mut store = wasmtime::Store::new(&engine, ());
+    let component =
+        wasmtime::component::Component::from_file(&engine, Path::new("wasm_comp_rs.wasm"))?;
+    let linker = wasmtime::component::Linker::new(&engine);
+    let bindings = Theworld::instantiate(&mut store, &component, &linker)?;
+    let res = bindings
+        .foo_stringoperations_theint()
+        .call_shmup(&mut store, "shmuppppp")?;
+    println!("result: {res}");
+    Ok(())
 }
