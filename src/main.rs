9eb0fde2defe5f8a1ed35ea92226c2e37f38636a
Johannes Maier <johannes.maier@mailbox.org>
2024-12-08 16:13:09 +0100

    stub out the replay entrypoint
    We can only stub it out right now, as we've gotten rid of the
    serialization of the event stream, hence cannot read any real replay
    from file.
    
    refs #15

--- src/main.rs
+++ src/main.rs
@@ -17,26 +17,6 @@ mod player;
 mod render;
 mod settings;
 
-fn run_replay(
-    history_file: &Path,
-    sender: mpsc::Sender<StepEvents>,
-    delay: &Duration,
-    cancel: &Arc<AtomicBool>,
-) -> Option<()> {
-    let f = std::fs::File::open(history_file).ok()?;
-    let steps: Vec<StepEvents> = todo!();
-    for step_events in steps {
-        if cancel.load(std::sync::atomic::Ordering::Relaxed) {
-            break;
-        }
-        sender
-            .send(step_events)
-            .expect("Failed sending step events");
-        std::thread::sleep(*delay);
-    }
-    Some(())
-}
-
 fn main() {
     let cli = cli::Cli::parse();
     match cli.mode {
@@ -45,6 +25,7 @@ fn main() {
             headless,
         } => {
             if headless {
+                // FIXME: unnecessary background thread
                 let game_thread = std::thread::spawn(move || -> Result<(), GameError> {
                     let mut game = Game::new();
                     for player_dir in player_dirs {
@@ -79,8 +60,8 @@ fn main() {
 
                 if game_thread.is_finished() {
                     match game_thread.join().unwrap() {
-                        Ok(_) => println!("game finished"),
-                        Err(e) => println!("error: {e}"),
+                        Ok(_) => println!("Battle finished"),
+                        Err(e) => println!("Battle crashed with error: {e}"),
                     }
                 } else {
                     cancel.store(true, std::sync::atomic::Ordering::Relaxed);
@@ -89,8 +70,54 @@ fn main() {
             }
         }
         cli::Mode::Replay { recording } => {
-            // run_replay(Path::new("events"), game_writer, &delay, &cancel_ref).unwrap();
-            todo!("show replay {recording:?}")
+            let (game_writer, game_reader) = mpsc::channel();
+            let cancel = Arc::new(AtomicBool::new(false));
+            let cancel_ref = cancel.clone();
+
+            let game_thread = std::thread::spawn(move || -> Option<()> {
+                let delay = Duration::from_millis(7);
+                run_replay(&recording, game_writer, &delay, &cancel_ref)
+            });
+
+            let (mut rl, thread) = raylib::init()
+                .size(WIDTH, HEIGHT)
+                .title("hello world")
+                .msaa_4x()
+                .build();
+            let mut renderer = GameRenderer::new(&game_reader);
+            while !rl.window_should_close() && !game_thread.is_finished() {
+                renderer.step(&mut rl, &thread);
+            }
+
+            if game_thread.is_finished() {
+                match game_thread.join().unwrap() {
+                    Some(_) => println!("Replay finished"),
+                    None => todo!("Something bad happened"),
+                }
+            } else {
+                cancel.store(true, std::sync::atomic::Ordering::Relaxed);
+                let _ = game_thread.join();
+            }
         }
     };
+}
+
+fn run_replay(
+    history_file: &Path,
+    sender: mpsc::Sender<StepEvents>,
+    delay: &Duration,
+    cancel: &Arc<AtomicBool>,
+) -> Option<()> {
+    let _f = std::fs::File::open(history_file).ok()?;
+    let steps: Vec<StepEvents> = vec![];
+    for step_events in steps {
+        if cancel.load(std::sync::atomic::Ordering::Relaxed) {
+            break;
+        }
+        sender
+            .send(step_events)
+            .expect("Failed sending step events");
+        std::thread::sleep(*delay);
+    }
+    Some(())
 }
